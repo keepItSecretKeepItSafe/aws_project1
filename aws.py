19baa7d2f984ceb68fdc6bad86e946781b4d6c01
Gandalf The Grey <drake.s.deaton.2@gmail.com>
2020-01-26 08:37:10 -0800

    Added more boto3 methods

--- aws.py
+++ aws.py
@@ -1,8 +1,19 @@
-
+import click
 import boto3
 
+session = boto3.Session(profile_name='drake')
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instance():
+	for i in ec2.instances.all():
+		print(', '.join((
+			i.id,
+			i.instance_type,
+			i.state['Name'],
+			i.public_dns_name)))
+	return
+
 if __name__ == '__main__':
-	session = boto3.Session(profile_name='drake')
-	ec2 = session.resource('ec2')
-	print(ec2.instances.all())
+	list_instance()
 
