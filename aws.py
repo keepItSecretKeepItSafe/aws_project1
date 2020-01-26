ad047016752aeeb7c2ded2ea5cfb758e15c46806
Gandalf The Grey <drake.s.deaton.2@gmail.com>
2020-01-26 10:38:24 -0800

    added starting and stopping instances functions

--- aws.py
+++ aws.py
@@ -4,16 +4,59 @@ import boto3
 session = boto3.Session(profile_name='drake')
 ec2 = session.resource('ec2')
 
-@click.command()
-def list_instance():
-	for i in ec2.instances.all():
+def filter_instances(project):
+	instances = []
+	if project:
+		filters = [{'Name':'tag:Project', 'Values':["test"]}]
+		instances = ec2.instances.filter(Filters=filters)
+	else:
+		instances = ec2.instances.all()
+	return instances
+
+@click.group()
+def instances():
+	"""Commands for instances"""
+
+@instances.command('list')
+@click.option('--project', default=None, help="Only instances for project (tag Project:<name>)")
+def list_instance(project):
+	"List EC2 instances"
+	instances = filter_instances(project)
+	for i in instances:
+		tags = {t['Key']: t['Value'] for t in i.tags or []}
 		print(', '.join((
-			i.id,
-			i.instance_type,
-			i.state['Name'],
-			i.public_dns_name)))
+			"\n" +
+			"id: " + i.id + "\n" +
+			"placement: " + i.placement['AvailabilityZone'] + "\n" +
+			"public_dns_name: " + i.public_dns_name + "\n" +
+			"tags: " + tags.get('Project', '<no tags>') + "\n" +
+			"instance_type: " + i.instance_type,
+			"\n")))
+	return
+
+@instances.command('stop')
+@click.option("--project", default=None, help="Only instances for project")
+def stop_instances(project):
+	"Stop EC2 instances"
+	instances = filter_instances(project)
+	for i in instances:
+		print("Stopping {0}...".format(i.id))
+		i.stop()
+	return
+
+
+@instances.command('start')
+@click.option("--project", default=None, help="Only instances for project")
+def start_instances(project):
+	"Start EC2 instances"
+	instances = filter_instances(project)
+	for i in instances:
+		print("Starting {0}...".format(i.id))
+		i.start()
 	return
 
 if __name__ == '__main__':
-	list_instance()
+	instances()
+
+
 
